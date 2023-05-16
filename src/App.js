c6bd5b4b3796fbe143108239422f2c000336075a
Mukul Ghanghas <partimag55@gmail.com>
2023-05-16 15:40:32 +0530

    commit init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./compoents/Navbar";
+import "./App.css";
+import Main from "./compoents/Main";
+import Footer from "./compoents/footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/compoents/footer.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="h22">
+        <h2 className="h2">Customer Feedback</h2>
+      </div>
+      <div className="diva">
+        <div className="div">
+          <p className="p"></p>
+          <h1>Customer Feedback</h1>
+          <h5>“Amazing product and company”</h5>
+          <p>
+            Lorem ipsum dolor sit amet, all consectetuer adipiscing elit.
+            allAenean commodo ligula eget dolor. Aenean massa. Cum sociis
+            natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus. for a Donec quam felis, ultricies nec, pellentesque.
+          </p>
+        </div>
+        <div className="div">
+          <img src="/icons/Ellipse 19.png" className="girl" alt="girl" />
+          <h1>Customer Feedback</h1>
+          <h5>“Amazing product and company”</h5>
+          <p>
+            Lorem ipsum dolor sit amet, all consectetuer adipiscing elit.
+            allAenean commodo ligula eget dolor. Aenean massa. Cum sociis
+            natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus. for a Donec quam felis, ultricies nec, pellentesque.
+          </p>
+        </div>
+        <div className="div">
+          <img src="/icons/Ellipse 19.png" className="girl" alt="girl" />
+
+          <h1>Customer Feedback</h1>
+          <h5>“Amazing product and company”</h5>
+          <p>
+            Lorem ipsum dolor sit amet, all consectetuer adipiscing elit.
+            allAenean commodo ligula eget dolor. Aenean massa. Cum sociis
+            natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus. for a Donec quam felis, ultricies nec, pellentesque.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
